85bf9b30269e224c7ca4256627ca7bed21652c97
Itadakimas <said.ahemt@gmail.com>
2015-12-11 19:46:29 +0100

    adds html task and templating

--- gulp/modules/config.js
+++ gulp/modules/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   clean: require("../../config/clean.json"),
+  jade: require("../../config/jade.json"),
   paths: require("../../config/paths.json"),
   sass: require("../../config/sass.json"),
   svgSprite: require("../../config/svg-sprite.json")
---
+++ gulp/tasks/html.js
@@ -0,0 +1,27 @@
+/*
+ * Node Dependencies
+ */
+var Gulp = require("gulp");
+var Jade = require("gulp-jade");
+
+/*
+ * Modules
+ */
+var argv = require("../modules/argv");
+var config = require("../modules/config");
+var replace = require("../modules/replace");
+var paths = require("../modules/paths");
+
+/*
+ * Task
+ */
+Gulp.task("html", function(callback) {
+
+  // TODO: add error handling
+
+  return Gulp
+    .src(paths.relocatePath(config.paths.sources.html))
+    .pipe(Jade(config.jade))
+    .pipe(replace)
+    .pipe(Gulp.dest(paths.relocatePath(config.paths.builds.html[argv.mode])));
+});
