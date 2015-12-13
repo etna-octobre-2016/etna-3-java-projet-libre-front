853bfe745c31f3bd64956af62072c9b321a7fcba
Saïd AHEMT <said.ahemt@nolaroads.com>
2015-12-13 12:13:46 +0100

    watches html / js / sass / svg file updates

--- gulp/tasks/dev.js
+++ gulp/tasks/dev.js
@@ -3,15 +3,33 @@
  */
 var Gulp = require("gulp");
 var RunSequence = require("run-sequence").use(Gulp);
+var Watch = require("gulp-watch");
+
+/*
+ * Modules
+ */
+var config = require("../modules/config");
+var paths = require("../modules/paths");
 
 /*
  * Task
  */
 Gulp.task("dev", function(callback) {
 
-  RunSequence("clean", "sass", "svg", "html", "javascript", "livereload", function(){
+  RunSequence("clean", "sass", "svg", "html", "javascript", "livereload", function() {
 
-
+    Watch(paths.relocate(config.common.paths.sources.html), function() {
+      RunSequence("html");
+    });
+    Watch(paths.relocate(config.common.paths.sources.js), function() {
+      RunSequence("js");
+    });
+    Watch(paths.relocate(config.common.paths.sources.sass), function() {
+      RunSequence("sass");
+    });
+    Watch(paths.relocate(config.common.paths.sources.svg), function() {
+      RunSequence("svg");
+    });
     callback();
   });
 });
