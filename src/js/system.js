b64d62418249a9f37f8c6a5180584c1df1b1b9cf
Itadakimas <said.ahemt@gmail.com>
2016-02-07 15:25:47 +0100

    fixes system.js paths

--- src/js/system.js
+++ src/js/system.js
@@ -1,18 +1,15 @@
-"use strict";
-
 System.config({
-  baseURL: "js",
   map: {
-    core: "core",
-    modules: "modules",
-    sections: "../../sections"
+    core: "js/core",
+    modules: "js/modules",
+    sections: "sections"
   },
   paths: {
-    eventEmitter: "../vendors/eventEmitter/EventEmitter.min.js",
-    json: "../vendors/plugin-json/json.js",
-    rlite: "../vendors/rlite/rlite.min.js",
-    text: "../vendors/plugin-text/text.js",
-    vue: "../vendors/vue/dist/vue.min.js"
+    eventEmitter: "vendors/eventEmitter/EventEmitter.min.js",
+    json: "vendors/plugin-json/json.js",
+    rlite: "vendors/rlite/rlite.min.js",
+    text: "vendors/plugin-text/text.js",
+    vue: "vendors/vue/dist/vue.min.js"
   }
 });
-System.import("main.js");
+System.import("js/main.js");
