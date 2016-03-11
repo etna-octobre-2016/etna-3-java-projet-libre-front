52c54019f2003c493a228668b7c032e37860f6af
Itadakimas <said.ahemt@gmail.com>
2016-03-11 15:59:17 +0100

    adds form validation

--- src/sections/signup/signup.js
+++ src/sections/signup/signup.js
@@ -5,7 +5,7 @@ import Events from "modules/core/events.js";
 import Vue from "vue";
 import template from "./signup.html";
 // import auth from "modules/api/auth.js";
-// import ui from "modules/ui/index.js";
+import ui from "modules/ui/index.js";
 
 var view;
 
@@ -22,58 +22,58 @@ export function init()
     ready: function() {
       
       Events.emit("section:loaded");
-      // this.formValidator = new window.Validatinator({
-      //   "loginForm": {
-      //     "email": "required|email",
-      //     "password": "required"
-      //   }
-      // });
+      this.formValidator = new window.Validatinator({
+        "signupForm": {
+          "email": "required|email",
+          "password": "required",
+          "passwordConfirm": "required|same:password",
+          "username": "required"
+        }
+      });
     },
     methods: {
       
       onFormSubmit: function(e) {
-        
+
+        var errors,
+            fieldNames,
+            fields,
+            form;
+            
         e.preventDefault();
-        console.log("on submit");
-        
-        // var errors,
-        //     fieldNames,
-        //     fields,
-        //     form;
-        //     
-        // e.preventDefault();
-        // form = new ui.form(this.$els.loginform);
-        // if (this.formValidator.fails("loginForm"))
-        // {
-        //   this.$els.loginform.setAttribute("data-state", "invalid");
-        //   errors = this.formValidator.errors.loginForm;
-        //   fields = form.getFields();
-        //   fieldNames = form.getFieldNames();
-        //   fieldNames.forEach(function(name) {
-        //     
-        //     if (typeof errors[name] !== "undefined")
-        //     {
-        //       fields[name].classList.add("is-invalid");
-        //     }
-        //     else
-        //     {
-        //       fields[name].classList.remove("is-invalid");
-        //     }
-        //   });
-        // }
-        // else
-        // {
-        //   this.$els.loginform.removeAttribute("data-state");
-        //   fields = form.getFields(true, true);
-        //   fields.forEach(function(f) {
-        //     
-        //     f.classList.remove("is-invalid");
-        //   });
-        //   auth.login(form.getData()).then(
-        //     this.onFormSubmitComplete.bind(this),
-        //     this.onFormSubmitError.bind(this)
-        //   );
-        // }
+        form = new ui.form(this.$els.signupform);
+        this.$els.signupform.removeAttribute("data-state");
+        if (this.formValidator.fails("signupForm"))
+        {
+          this.$els.signupform.setAttribute("data-state", "invalid");
+          errors = this.formValidator.errors.signupForm;
+          fields = form.getFields();
+          fieldNames = form.getFieldNames();
+          fieldNames.forEach(function(name) {
+            
+            if (typeof errors[name] !== "undefined")
+            {
+              fields[name].classList.add("is-invalid");
+            }
+            else
+            {
+              fields[name].classList.remove("is-invalid");
+            }
+          });
+        }
+        else
+        {
+          fields = form.getFields(true, true);
+          fields.forEach(function(f) {
+            
+            f.classList.remove("is-invalid");
+          });
+          console.log("requête");
+          // auth.login(form.getData()).then(
+          //   this.onFormSubmitComplete.bind(this),
+          //   this.onFormSubmitError.bind(this)
+          // );
+        }
       },
       onFormSubmitComplete: function(details) {
         
