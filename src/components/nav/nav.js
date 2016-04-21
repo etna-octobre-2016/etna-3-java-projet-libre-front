1232953a86aeaa9949cba59dd24056fdfb1bcdd1
Itadakimas <said.ahemt@gmail.com>
2016-04-21 15:09:50 +0200

    adds nav add button interactions

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,10 +1,48 @@
+import Vue from "vue";
 import template from "./nav.html";
+import navTodo from "components/nav-todo/nav-todo.js";
 
 export default {
 
   template: template,
-  ready: function() {
+  components: {
+    "component-nav-todo": navTodo
+  },
+  data: function() {
 
-    console.log("nav component ready");
+    return {
+
+      isAddingTodo: false,
+      todos: []
+    };
+  },
+  methods: {
+
+    addTodoList: function(todo) {
+
+      console.log("addTodoList");
+      console.log(todo);
+    },
+    onAddBtnClick: function() {
+
+      this.isAddingTodo = true;
+      Vue.nextTick(function() {
+
+        this.$els.input.focus();
+
+      }.bind(this));
+    },
+    onAddComplete: function(e) {
+
+      var todoLabel;
+
+      todoLabel = e.target.value;
+      if (todoLabel.length > 0)
+      {
+        this.addTodoList({ label: todoLabel });
+        e.target.value = "";
+      }
+      this.isAddingTodo = false;
+    }
   }
 };
