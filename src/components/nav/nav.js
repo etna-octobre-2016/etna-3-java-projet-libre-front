e34605c0ac3620d0f89f6b05a8e47fd479e6999b
Itadakimas <said.ahemt@gmail.com>
2016-04-21 16:21:40 +0200

    fetches task categories on load and sends creation request

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import TaskCategories from "modules/api/task-categories.js";
 import template from "./nav.html";
 import navItem from "components/nav-item/nav-item.js";
 
@@ -16,11 +17,21 @@ export default {
       categories: []
     };
   },
+  created: function() {
+
+    TaskCategories.fetchAll().then(
+      this.onTaskCategoriesFetchSuccess.bind(this),
+      this.onTaskCategoriesFetchError.bind(this)
+    );
+  },
   methods: {
 
-    addCategory: function(todo) {
+    addCategory: function(category) {
 
-      this.categories.push(todo);
+      TaskCategories.create(category).then(
+        this.onTaskCategoriesCreateSuccess.bind(this, category),
+        this.onTaskCategoriesCreateError.bind(this)
+      );
     },
     onAddBtnClick: function() {
 
@@ -38,10 +49,28 @@ export default {
       categoryLabel = e.target.value;
       if (categoryLabel.length > 0)
       {
-        this.addCategory({ label: categoryLabel });
+        this.addCategory({ name: categoryLabel });
         e.target.value = "";
       }
       this.isAddingCategory = false;
+    },
+    onTaskCategoriesCreateError: function(e) {
+
+      console.error("Une erreur a eu lieu lors de la création d'une catégorie de tâches. Voir exception ci-dessous :");
+      console.log(e);
+    },
+    onTaskCategoriesCreateSuccess: function(category) {
+
+      this.categories.push(category);
+    },
+    onTaskCategoriesFetchError: function(e) {
+
+      console.error("Une erreur a eu lieu lors de la récupération des catégories de tâches. Voir exception ci-dessous :");
+      console.log(e);
+    },
+    onTaskCategoriesFetchSuccess: function(response) {
+
+      this.categories = response.data;
     }
   }
 };
